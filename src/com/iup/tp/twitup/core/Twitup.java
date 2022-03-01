822338daa77e7f73b08da70ea053b5d9a0780451
vukans <the.salamaders@gmail.com>
2022-03-01 09:39:51 +0100

    premier commit

---
+++ src/com/iup/tp/twitup/core/Twitup.java
@@ -0,0 +1,143 @@
+package com.iup.tp.twitup.core;
+
+import java.io.File;
+
+import com.iup.tp.twitup.datamodel.Database;
+import com.iup.tp.twitup.datamodel.IDatabase;
+import com.iup.tp.twitup.events.file.IWatchableDirectory;
+import com.iup.tp.twitup.events.file.WatchableDirectory;
+import com.iup.tp.twitup.ihm.TwitupMainView;
+import com.iup.tp.twitup.ihm.TwitupMock;
+
+/**
+ * Classe principale l'application.
+ * 
+ * @author S.Lucas
+ */
+public class Twitup {
+	/**
+	 * Base de données.
+	 */
+	protected IDatabase mDatabase;
+
+	/**
+	 * Gestionnaire des entités contenu de la base de données.
+	 */
+	protected EntityManager mEntityManager;
+
+	/**
+	 * Vue principale de l'application.
+	 */
+	protected TwitupMainView mMainView;
+
+	/**
+	 * Classe de surveillance de répertoire
+	 */
+	protected IWatchableDirectory mWatchableDirectory;
+
+	/**
+	 * Répertoire d'échange de l'application.
+	 */
+	protected String mExchangeDirectoryPath;
+
+	/**
+	 * Idnique si le mode bouchoné est activé.
+	 */
+	protected boolean mIsMockEnabled = false;
+
+	/**
+	 * Nom de la classe de l'UI.
+	 */
+	protected String mUiClassName;
+
+	/**
+	 * Constructeur.
+	 */
+	public Twitup() {
+		// Init du look and feel de l'application
+		this.initLookAndFeel();
+
+		// Initialisation de la base de données
+		this.initDatabase();
+
+		if (this.mIsMockEnabled) {
+			// Initialisation du bouchon de travail
+			this.initMock();
+		}
+
+		// Initialisation de l'IHM
+		this.initGui();
+
+		// Initialisation du répertoire d'échange
+		this.initDirectory();
+	}
+
+	/**
+	 * Initialisation du look and feel de l'application.
+	 */
+	protected void initLookAndFeel() {
+	}
+
+	/**
+	 * Initialisation de l'interface graphique.
+	 */
+	protected void initGui() {
+		// this.mMainView...
+	}
+
+	/**
+	 * Initialisation du répertoire d'échange (depuis la conf ou depuis un file
+	 * chooser). <br/>
+	 * <b>Le chemin doit obligatoirement avoir été saisi et être valide avant de
+	 * pouvoir utiliser l'application</b>
+	 */
+	protected void initDirectory() {
+	}
+
+	/**
+	 * Indique si le fichier donné est valide pour servire de répertoire
+	 * d'échange
+	 * 
+	 * @param directory
+	 *            , Répertoire à tester.
+	 */
+	protected boolean isValideExchangeDirectory(File directory) {
+		// Valide si répertoire disponible en lecture et écriture
+		return directory != null && directory.exists() && directory.isDirectory() && directory.canRead()
+				&& directory.canWrite();
+	}
+
+	/**
+	 * Initialisation du mode bouchoné de l'application
+	 */
+	protected void initMock() {
+		TwitupMock mock = new TwitupMock(this.mDatabase, this.mEntityManager);
+		mock.showGUI();
+	}
+
+	/**
+	 * Initialisation de la base de données
+	 */
+	protected void initDatabase() {
+		mDatabase = new Database();
+		mEntityManager = new EntityManager(mDatabase);
+	}
+
+	/**
+	 * Initialisation du répertoire d'échange.
+	 * 
+	 * @param directoryPath
+	 */
+	public void initDirectory(String directoryPath) {
+		mExchangeDirectoryPath = directoryPath;
+		mWatchableDirectory = new WatchableDirectory(directoryPath);
+		mEntityManager.setExchangeDirectory(directoryPath);
+
+		mWatchableDirectory.initWatching();
+		mWatchableDirectory.addObserver(mEntityManager);
+	}
+
+	public void show() {
+		// ... setVisible?
+	}
+}
---
+++ src/com/iup/tp/twitup/ihm/TwitupMainView.java
@@ -0,0 +1,7 @@
+package com.iup.tp.twitup.ihm;
+
+/**
+ * Classe de la vue principale de l'application.
+ */
+public class TwitupMainView {
+}
